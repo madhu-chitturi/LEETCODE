a884c8f5904a5e99489583f915d4acb565b67b7a
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2023-01-05 22:03:03 +0530

    69. Sqrt(x).py

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,6 @@
+                                                                69. Sqrt(x)                             
+  
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(math.sqrt(x))
