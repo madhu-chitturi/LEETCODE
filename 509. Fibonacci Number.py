c869938a9c2744b2151e2dfb5ca460c19eee3702
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2023-01-13 13:09:22 +0530

    509. Fibonacci Number.py

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    if(n==0 or n==1):
+        return n
+    return fibonacci(n-1)+fibonacci(n-2)
+
+class Solution:
+    def fib(self, n: int) -> int:
+        return fibonacci(n)
